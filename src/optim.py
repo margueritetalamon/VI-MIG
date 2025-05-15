25af402eb1358ce6ae6f547822478f9eaf88d32c
Louis Montaut <louismontaut@gmail.com>
2025-05-15 14:11:01 +0200

    optim: fix assertion

--- src/optim.py
+++ src/optim.py
@@ -207,7 +207,7 @@ class VI_GMM:
                     X_ = torch.from_numpy(self.target.model.X)
                     y_ = torch.from_numpy(self.target.model.y)
                     #
-                    M_test = 1000
+                    M_test = math.min(1000, self.target.model.X_test.shape[0])
                     indices = np.random.permutation(len(self.target.model.X_test))[:M_test]
                     X_test = self.target.model.X_test[indices]
                     y_test = self.target.model.y_test[indices]
@@ -256,7 +256,7 @@ class VI_GMM:
                     assert y_hat_test.shape[0] == N_test
                     assert y_hat_test.size == N_test
                     #
-                    y_test = self.target.model.y_test.argmax(axis = -1)
+                    y_test = y_test.argmax(axis = -1)
                     assert y_test.shape[0] == N_test
                     assert y_test.size == N_test
                     #
